c770303bc4cf02e0f016f9cf9d60a8f53b181591
Cromsies <childofbodom87@gmail.com>
2014-05-03 15:48:40 -0400

    ADDED  script
    oi vey

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        rigidbody.AddForce(Vector3);
+    }
+
+
+}
